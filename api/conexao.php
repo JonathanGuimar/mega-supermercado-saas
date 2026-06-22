fcb048631a380496b3b64de54b6399ccddcbd212
JonathanGuimar <107708396+JonathanGuimar@users.noreply.github.com>
2026-06-22 19:17:55 -0300

    Update database connection credentials and options

--- api/conexao.php
+++ api/conexao.php
@@ -1,16 +1,16 @@
 <?php
-// Credenciais reais do seu TiDB Cloud
+// Credenciais do TiDB Cloud
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"; 
-$banco = "mega_supermercado"; 
+$banco = "test"; // Mudamos para 'test' que já vem criado por padrão no TiDB
 $usuario = "4AD79M25udvWBbf.root"; 
 $senha = "ox5Jhw6PRooMOMGC"; 
 $porta = 4000; 
 
 try {
-    // Configuração obrigatória para o TiDB aceitar a conexão segura na Vercel
+    // Usando os números diretos (1007 e 1008) sumimos com os avisos de "Deprecated"
     $opcoes = [
-        PDO::MYSQL_ATTR_SSL_CA => true,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+        1007 => true,  // PDO::MYSQL_ATTR_SSL_CA
+        1008 => false  // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT
     ];
 
     $pdo = new PDO("mysql:host=$host;port=$porta;dbname=$banco;charset=utf8", $usuario, $senha, $opcoes);
