c219cbc5215c526076e7f998527473f5a8879757
JonathanGuimar <107708396+JonathanGuimar@users.noreply.github.com>
2026-06-22 19:19:24 -0300

    Refactor PDO SSL options for database connection
    Updated SSL options for PDO connection to avoid deprecated warnings.

--- api/conexao.php
+++ api/conexao.php
@@ -1,16 +1,16 @@
 <?php
 // Credenciais do TiDB Cloud
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"; 
-$banco = "test"; // Mudamos para 'test' que já vem criado por padrão no TiDB
+$banco = "test"; 
 $usuario = "4AD79M25udvWBbf.root"; 
 $senha = "ox5Jhw6PRooMOMGC"; 
 $porta = 4000; 
 
 try {
-    // Usando os números diretos (1007 e 1008) sumimos com os avisos de "Deprecated"
+    // Compatibilidade universal para o SSL na Vercel sem gerar avisos de Deprecated
     $opcoes = [
-        1007 => true,  // PDO::MYSQL_ATTR_SSL_CA
-        1008 => false  // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT
+        "mysql_attr_ssl_ca" => true,
+        "mysql_attr_ssl_verify_server_cert" => false
     ];
 
     $pdo = new PDO("mysql:host=$host;port=$porta;dbname=$banco;charset=utf8", $usuario, $senha, $opcoes);
