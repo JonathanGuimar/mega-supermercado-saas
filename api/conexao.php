56be7c37873eaec10acdb6116838543d1a1a9a10
JonathanGuimar <107708396+JonathanGuimar@users.noreply.github.com>
2026-06-22 19:24:02 -0300

    Update conexao.php

--- api/conexao.php
+++ api/conexao.php
@@ -1,16 +1,16 @@
 <?php
-// Credenciais do TiDB Cloud
+// Credenciais do TiDB Cloud corrigidas
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"; 
-$banco = "test"; 
+$banco = "mega_supermercado"; 
 $usuario = "4AD79M25udvWBbf.root"; 
 $senha = "ox5Jhw6PRooMOMGC"; 
 $porta = 4000; 
 
 try {
-    // Usando o escopo correto da classe Mysql para ativar o SSL no PHP moderno da Vercel
+    // Força o SSL usando as constantes nativas da Vercel
     $opcoes = [
-        \PDO\Mysql::ATTR_SSL_CA => true,
-        \PDO\Mysql::ATTR_SSL_VERIFY_SERVER_CERT => false
+        PDO::MYSQL_ATTR_SSL_CA => true,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
     ];
 
     $pdo = new PDO("mysql:host=$host;port=$porta;dbname=$banco;charset=utf8", $usuario, $senha, $opcoes);
